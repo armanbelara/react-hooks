77665824699b9b48b174c53410d14adcff10abf7
armanbelara <arman@paymongo.com>
2022-06-07 19:02:49 +0800

    Apply useEffect

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useForm } from "./useForm";
+import { useFetch } from "./useFetch";
 
 const App = () => {
-  const [values, handleChange] = useForm({email: '', password: ''});
+  const [values, handleChange] = useForm({
+    email: '',
+    password: '',
+    firstName: ''
+  });
+  const [count, setCount] = useState(() => 
+    JSON.parse(localStorage.getItem('count')) && 0
+  );
+  const {data} = useFetch(`http://numbersapi.com/${count}/trivia`);
+
+  useEffect(() => {
+    localStorage.setItem("count", JSON.stringify(count));
+  }, [count]);
 
   return (
     <div>
-      <input
-        name="email"
-        value={values.email}
-        onChange={handleChange}
-      />
-      <input
-        type="password"
-        name="password"
-        value={values.password}
-        onChange={handleChange}
-      />
+      <div>{!data ? 'loading...' : data}</div>
+      <div>count: {count}</div>
+      <button onClick={() => {setCount(c => c + 1)}}>increment</button>
+      <>
+        <input
+          name="email"
+          value={values.email}
+          onChange={handleChange}
+        />
+        <input
+          name="firstName"
+          placeholder="Firstname"
+          value={values.firstName}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          value={values.password}
+          onChange={handleChange}
+        />
+      </>
     </div>
   );
 }
